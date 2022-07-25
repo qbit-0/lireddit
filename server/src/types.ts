587c60243a4b4a7149be76dd0306bfc8a16fbeab
Duy Pham <duypham12241999@gmail.com>
2022-07-25 16:48:58 -0700

    remove mikro-orm

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,4 +1,3 @@
-import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 import { Request, Response } from "express";
 import { Session } from "express-session";
 import { Redis } from "ioredis";
